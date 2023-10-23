7645d4e508ea6a6c63605f64ba0ab5ce89cbfdff
FazilatTuxtasinova <147168697+FazilatTuxtasinova@users.noreply.github.com>
2023-10-23 19:59:31 +0500

    JS 2-dars

---
+++ JS 2-dars/js/main.js
@@ -0,0 +1,80 @@
+/*
+if(agar)
+else if(yoki bo`lmasa)
+else(u holda)
+ */
+/*if(44>='45'){
+    console.log("Ular teng!");
+}
+else{
+    console.log("Ular teng emas!");
+}*/
+
+/*if(45 == 44){
+    console.log("Ular teng!-1")
+}
+else if(45 == 46){
+    console.log("Ular teng!-2")
+}
+else{
+    console.log("Yuqoridagilarning hech biri teng emas!");
+}*/
+/*
+if("16" == "16"){
+    console.log("Bir xil!");
+}
+else{
+    console.log("Bir xil emas!");
+}
+if(54 == 56){
+    console.log("Bir xil!-1");
+}
+else if(901 == 902){
+    console.log("Bir xil!-2");
+}
+else{
+    console.log("Hech biri bir xil emas!");
+}
+if("hello" == "bye"){
+    console.log("Bir xil!");
+}
+else{
+    console.log("Bir xil emas!");
+}
+if("xayr == salom"){
+    console.log("Bir xil!-1");
+}
+else if("IT == IT"){
+    console.log("Bir xil!-2");
+}
+else{
+    console.log("Hech biri bir xil emas!");
+}*/
+let a = 10;
+let b = 46;
+let c = 49;
+let d = 55;
+/*if(a == 1 && b == 46 && c == 49 && d == 55){
+    console.log("Solishtirilgan qiymatlarning hammasi to`g`ri!");
+}
+else{
+    console.log("Solishtirilgan qiymatlarning kamida bittasi noto`g`ri!");
+}*/
+
+/*
+if(a == 8 || b == 45 || c == 48 || d == 54){
+    console.log("Solishtirilgan qiymatlarning kamida bittasi to`g`ri!");
+}
+else {
+    console.log("Solishtirilgan qiymatlarning hammasi noto`g`ri!");
+}*/
+/*
+let matn = "JavaScript"; /!*string*!/
+let son = 45   /!*number*!/
+*/
+/*
+document.write(typeof son)*/
+/*let rost = true;*/ /*boolean*/
+let rost = 45 == 45 ? true:false;
+
+document.write(typeof rost);
